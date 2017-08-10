92a84b34b897dcbb6d5269995fe929724e42c29a
Kevan Atkins <kevan.atkins@gmail.com>
2017-08-11 01:40:16 +1000

    Breaking form JSON schemas out into files.

--- public/assets/javascripts/submission.js
+++ public/assets/javascripts/submission.js
@@ -26140,191 +26140,7 @@ module.exports = ReactDOMInvalidARIAHook;
 
 
 
-
-const schema = {
-  title: "Project Submissions",
-  description: "Submissions for the 2019 Tilde festival",
-  type: "object",
-  required: ["name", "email", "confirm_email", "phone_number", "discipline", "artistBio", "cv", "project_title", "short_description", "long_description", "duration", "technical_requirements", "your_equipment", "tilde_equipment", "setup_time", "outsideOfAustralia", "soughtFunding", "focus_areas", "links"],
-  properties: {
-    name: {
-      type: "string",
-      title: "Name"
-    },
-    email: {
-      type: "string",
-      format: "email",
-      title: "Email",
-      description: "This information will be used for correspondance with the Tilde team and will not be disseminated to others."
-    },
-    confirm_email: {
-      type: "string",
-      format: "email",
-      title: "Confirm Email"
-    },
-    phone_number: {
-      type: "string",
-      title: "Phone Number",
-      description: "This information will be used for correspondance with the Tilde team and will not be disseminated to others."
-    },
-    discipline: {
-      type: "string",
-      title: "Discipline",
-      "enum": ["performance", "installation", "exhibition", "other"],
-      "enumNames": ["Performance", "Installation", "Exhibition", "Other"]
-    },
-    artistBio: {
-      type: "string",
-      title: "Artist Bio (up to 500 words)",
-      description: "Give us an idea of who you are, including: your practice and artistic interests. This will be used as programming promotional material and for peer assessment."
-    },
-    cv: {
-      type: "string",
-      title: "Brief CV",
-      description: "List major career achievements and developments."
-    },
-    project_title: {
-      type: "string",
-      title: "Project Title"
-    },
-    short_description: {
-      type: "string",
-      title: "Short description/Program notes",
-      description: "Program note. This will be used for promotion as well as the festival program"
-    },
-    long_description: {
-      type: "string",
-      title: "Long description (up to 500 words)",
-      description: "Describe your project in more detail. How you will deliver it? Include repertoire (if relevant). This information will help us to determine whether or not Tilde can facilitate the project."
-    },
-    duration: {
-      type: "string",
-      title: "Duration"
-    },
-    technical_requirements: {
-      type: "string",
-      title: "Technical Requirements",
-      description: "Description of setup"
-    },
-    your_equipment: {
-      type: "string",
-      title: "Equipment you will supply"
-    },
-    tilde_equipment: {
-      type: "string",
-      title: "Equipment that you would like Tilde to supply								"
-    },
-    setup_time: {
-      type: "string",
-      title: "Setup Time",
-      description: "Let us know how much time you will need to set up and if you will need access to the site before the day."
-    },
-    instrumentation: {
-      type: "string",
-      title: "Instrumentation (if relevant)",
-      description: "Please make a note if an instrument requires amplification."
-    },
-    outsideOfAustralia: {
-      type: "boolean",
-      title: "Outside Of Australia"
-    },
-    soughtFunding: {
-      type: "string",
-      title: "Sought Funding",
-      description: "Please give details of the submission and notification dates."
-    },
-    focus_areas: {
-      type: "string",
-      title: "Focus Areas",
-      description: "Please provide a brief description of how your project addresses these areas. This information will be used by the assessors and will not appear in the program. NB: Site-specific works should go further than simply reacting to the site through improvisation. Artists are encouraged to visit Testing Grounds and discuss possibilities with the Tilde team (info@tilde.net.au)"
-    },
-    links: {
-      type: "string",
-      title: "Link(s) to support material",
-      description: "Show us an example of your work"
-    },
-    comments: {
-      type: "string",
-      title: "Comments",
-      description: "Comments/other information"
-    }
-  }
-};
-
-const uiSchema = {
-  "artistBio": {
-    "ui:widget": "textarea",
-    "ui:options": {
-      "rows": 5
-    }
-  },
-  "cv": {
-    "ui:widget": "textarea",
-    "ui:options": {
-      "rows": 5
-    }
-  },
-  "short_description": {
-    "ui:widget": "textarea",
-    "ui:options": {
-      "rows": 5
-    }
-  },
-  "long_description": {
-    "ui:widget": "textarea",
-    "ui:options": {
-      "rows": 5
-    }
-  },
-  "technical_requirements": {
-    "ui:widget": "textarea",
-    "ui:options": {
-      "rows": 5
-    }
-  },
-  "your_equipment": {
-    "ui:widget": "textarea",
-    "ui:options": {
-      "rows": 5
-    }
-  },
-  "tilde_equipment": {
-    "ui:widget": "textarea",
-    "ui:options": {
-      "rows": 5
-    }
-  },
-  "outsideOfAustralia": {
-    "ui:widget": "radio"
-  },
-  "discipline": {
-    "ui:widget": "radio"
-  },
-  "soughtFunding": {
-    "ui:widget": "textarea",
-    "ui:options": {
-      "rows": 5
-    }
-  },
-  "focus_areas": {
-    "ui:widget": "textarea",
-    "ui:options": {
-      "rows": 5
-    }
-  },
-  "links": {
-    "ui:widget": "textarea",
-    "ui:options": {
-      "rows": 5
-    }
-  },
-  "comments": {
-    "ui:widget": "textarea",
-    "ui:options": {
-      "rows": 5
-    }
-  }
-};
+const schemas = __webpack_require__(279);
 
 function validate(formData, errors) {
   if (formData.email !== formData.confirm_email) {
@@ -26352,8 +26168,8 @@ const onSubmit = ({ formData }) => {
 
 class App extends __WEBPACK_IMPORTED_MODULE_0_react__["Component"] {
   render() {
-    return __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_1_react_jsonschema_form___default.a, { schema: schema,
-      uiSchema: uiSchema,
+    return __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_1_react_jsonschema_form___default.a, { schema: schemas.schema,
+      uiSchema: schemas.uiSchema,
       validate: validate,
       onSubmit: onSubmit,
       onChange: log("changed"),
@@ -34926,6 +34742,226 @@ module.exports = function spread(callback) {
   };
 };
 
+
+/***/ }),
+/* 279 */
+/***/ (function(module, exports) {
+
+module.exports = {
+	"schema": {
+		"title": "Project Submissions",
+		"description": "Submissions for the 2019 Tilde festival",
+		"type": "object",
+		"required": [
+			"name",
+			"email",
+			"confirm_email",
+			"phone_number",
+			"discipline",
+			"artistBio",
+			"cv",
+			"project_title",
+			"short_description",
+			"long_description",
+			"duration",
+			"technical_requirements",
+			"your_equipment",
+			"tilde_equipment",
+			"setup_time",
+			"outsideOfAustralia",
+			"soughtFunding",
+			"focus_areas",
+			"links"
+		],
+		"properties": {
+			"name": {
+				"type": "string",
+				"title": "Name"
+			},
+			"email": {
+				"type": "string",
+				"format": "email",
+				"title": "Email",
+				"description": "This information will be used for correspondance with the Tilde team and will not be disseminated to others."
+			},
+			"confirm_email": {
+				"type": "string",
+				"format": "email",
+				"title": "Confirm Email"
+			},
+			"phone_number": {
+				"type": "string",
+				"title": "Phone Number",
+				"description": "This information will be used for correspondance with the Tilde team and will not be disseminated to others."
+			},
+			"discipline": {
+				"type": "string",
+				"title": "Discipline",
+				"enum": [
+					"performance",
+					"installation",
+					"exhibition",
+					"other"
+				],
+				"enumNames": [
+					"Performance",
+					"Installation",
+					"Exhibition",
+					"Other"
+				]
+			},
+			"artistBio": {
+				"type": "string",
+				"title": "Artist Bio (up to 500 words)",
+				"description": "Give us an idea of who you are, including: your practice and artistic interests. This will be used as programming promotional material and for peer assessment."
+			},
+			"cv": {
+				"type": "string",
+				"title": "Brief CV",
+				"description": "List major career achievements and developments."
+			},
+			"project_title": {
+				"type": "string",
+				"title": "Project Title"
+			},
+			"short_description": {
+				"type": "string",
+				"title": "Short description/Program notes",
+				"description": "Program note. This will be used for promotion as well as the festival program"
+			},
+			"long_description": {
+				"type": "string",
+				"title": "Long description (up to 500 words)",
+				"description": "Describe your project in more detail. How you will deliver it? Include repertoire (if relevant). This information will help us to determine whether or not Tilde can facilitate the project."
+			},
+			"duration": {
+				"type": "string",
+				"title": "Duration"
+			},
+			"technical_requirements": {
+				"type": "string",
+				"title": "Technical Requirements",
+				"description": "Description of setup"
+			},
+			"your_equipment": {
+				"type": "string",
+				"title": "Equipment you will supply"
+			},
+			"tilde_equipment": {
+				"type": "string",
+				"title": "Equipment that you would like Tilde to supply"
+			},
+			"setup_time": {
+				"type": "string",
+				"title": "Setup Time",
+				"description": "Let us know how much time you will need to set up and if you will need access to the site before the day."
+			},
+			"instrumentation": {
+				"type": "string",
+				"title": "Instrumentation (if relevant)",
+				"description": "Please make a note if an instrument requires amplification."
+			},
+			"outsideOfAustralia": {
+				"type": "boolean",
+				"title": "Outside Of Australia"
+			},
+			"soughtFunding": {
+				"type": "string",
+				"title": "Sought Funding",
+				"description": "Please give details of the submission and notification dates."
+			},
+			"focus_areas": {
+				"type": "string",
+				"title": "Focus Areas",
+				"description": "Please provide a brief description of how your project addresses these areas. This information will be used by the assessors and will not appear in the program. NB: Site-specific works should go further than simply reacting to the site through improvisation. Artists are encouraged to visit Testing Grounds and discuss possibilities with the Tilde team (info@tilde.net.au)"
+			},
+			"links": {
+				"type": "string",
+				"title": "Link(s) to support material",
+				"description": "Show us an example of your work"
+			},
+			"comments": {
+				"type": "string",
+				"title": "Comments",
+				"description": "Comments/other information"
+			}
+		}
+	},
+	"uiSchema": {
+		"artistBio": {
+			"ui:widget": "textarea",
+			"ui:options": {
+				"rows": 5
+			}
+		},
+		"cv": {
+			"ui:widget": "textarea",
+			"ui:options": {
+				"rows": 5
+			}
+		},
+		"short_description": {
+			"ui:widget": "textarea",
+			"ui:options": {
+				"rows": 5
+			}
+		},
+		"long_description": {
+			"ui:widget": "textarea",
+			"ui:options": {
+				"rows": 5
+			}
+		},
+		"technical_requirements": {
+			"ui:widget": "textarea",
+			"ui:options": {
+				"rows": 5
+			}
+		},
+		"your_equipment": {
+			"ui:widget": "textarea",
+			"ui:options": {
+				"rows": 5
+			}
+		},
+		"tilde_equipment": {
+			"ui:widget": "textarea",
+			"ui:options": {
+				"rows": 5
+			}
+		},
+		"outsideOfAustralia": {
+			"ui:widget": "radio"
+		},
+		"discipline": {
+			"ui:widget": "radio"
+		},
+		"soughtFunding": {
+			"ui:widget": "textarea",
+			"ui:options": {
+				"rows": 5
+			}
+		},
+		"focus_areas": {
+			"ui:widget": "textarea",
+			"ui:options": {
+				"rows": 5
+			}
+		},
+		"links": {
+			"ui:widget": "textarea",
+			"ui:options": {
+				"rows": 5
+			}
+		},
+		"comments": {
+			"ui:widget": "textarea",
+			"ui:options": {
+				"rows": 5
+			}
+		}
+	}
+};
 
 /***/ })
 /******/ ]);
